d38bf409315d82718e47bb114a9c6a2bedee6b5f
kevin <k86359303@gmail.com>
2025-06-16 23:07:37 +0800

    Update JDBC.java

--- JDBC.java
+++ JDBC.java
@@ -16,18 +16,13 @@ public class JDBC {
             conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/wuhan","postgres","gis2023");
             System.out.println("连接数据库成功!");
             //3.获取执行sql的对象
-            stmt = conn.createStatement();
+
             //4.定义sql
-            String sql = "SELECT name,ST_AsGeoJSON(geom) AS geojson  FROM res1_4m LIMIT 1";
+
             //5.执行sql
-            rs = stmt.executeQuery(sql);
+
             //6.遍历结果集，封装对象，装载集合
-            while(rs.next()){
-                //获取数据
-                String name = rs.getString("name");
-                String json = rs.getString("geojson");
-                System.out.println("name:"+name+" geojson:"+json);
-            }
+
             //7.释放资源
             rs.close();
             stmt.close();
